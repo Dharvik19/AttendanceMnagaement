fc7b2b5413b8aa8ec8e95a7245dfd93c943c6ac8
Dharvik19 <dharvik.badga22@gmail.com>
2023-11-26 17:35:41 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+
+
+const port = 8081;
+
+app.listen(port, ()=>{
+    console.log(`server running on port : ${port}`);
+})
+
